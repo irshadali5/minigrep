9dda7351d09451e1b0e1878c3e2bc58633cfe6ba
Irshad Ali <irshadali5@proton.me>
2026-06-20 06:27:46 +0530

    feat: initial minigrep implementation from chapter 12

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use minigrep::Config;
+use minigrep::{search, search_case_insensitive};
+use std::env;
+use std::error::Error;
+use std::fs;
+use std::process;
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+    println!("{:?}", config);
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+    for line in results {
+        println!("{line}");
+    }
+    Ok(())
+}
